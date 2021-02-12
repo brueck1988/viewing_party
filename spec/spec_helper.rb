4fcf4a662332b1f9fb95960e4f707120c1d8ea63
ian douglas <ian.douglas@iandouglas.com>
2021-02-12 09:21:27 -0700

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,7 @@
 #
 require 'simplecov'
 SimpleCov.start 'rails'
+SimpleCov.add_filter ['spec', 'config']
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
