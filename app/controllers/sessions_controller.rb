4912e0496762e7e1b31c3beb95784537e59361a8
r-stemmle <ryan.stemmle@gmail.com>
2021-05-11 19:34:32 -0400

    Add Create action to sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:info] = "Welcome, #{user.email}!"
+      redirect_to dashboard_path(user.id)
+    else
+      flash[:error] = "Your email or password are incorrect"
+      redirect_to root_path
+    end
   end
 end
