7287508d672325aeafa8252461e815f4085ff76e
r-stemmle <ryan.stemmle@gmail.com>
2021-05-11 17:04:24 -0400

    Fix User validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
-RSpec.describe "User" do
+
+RSpec.describe User, type: :model do
   describe "validations" do
     it {should validate_presence_of :email}
     it {should validate_uniqueness_of :email}
