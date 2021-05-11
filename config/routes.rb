26e808fcab8cfc2b54f12bbd30a28a60bb676091
r-stemmle <ryan.stemmle@gmail.com>
2021-05-11 19:36:05 -0400

    Add dashboard controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   root "welcome#index"
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
+  resources :dashboard, only: :show
 end
--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -11,4 +11,48 @@ RSpec.describe "Welcome page" do
       expect(page).to have_link("New to Viewing Party? Register Here")
     end
   end
+
+  describe "logging in" do
+    it "As a registered user I can login with correct credentials" do
+      user = User.create(email: "john@appleseed.com", password: "12345" )
+
+      visit root_path
+
+      fill_in :email, with: "john@appleseed.com"
+      fill_in :password, with: "12345"
+
+      click_button("Log In")
+
+      expect(current_path).to eq(dashboard_path(user.id))
+      expect(page).to have_content("Welcome, john@appleseed.com!")
+      expect(page).to_not have_link("I already have an account")
+      expect(page).to_not have_link("Sign up to be a user")
+      expect(page).to have_link("Log Out")
+
+      click_link("Log Out")
+
+      expect(current_path).to eq(root_path)
+      expect(page).to_not have_link("Log Out")
+      expect(page).to have_link("I already have an account")
+      expect(page).to have_link("Sign up to be a user ")
+    end
+
+    it "As a registered user, I can't log in with incorrect credentials" do
+      user = User.create(email: "john@appleseed.com", password: "12345" )
+
+      visit root_path
+
+      click_link("I already have an account")
+
+      expect(current_path).to eq(new_session_path)
+
+      fill_in :email, with: "john@appleseed.com"
+      fill_in :password, with: "123456"
+
+      click_button("Log In")
+
+      expect(current_path).to eq(root_path)
+      expect(page).to have_content("Your email or password are incorrect")
+    end
+  end
 end
