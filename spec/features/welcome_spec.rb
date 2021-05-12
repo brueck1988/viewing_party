d2367fbdb0093eb38a2be8c47ef4bd02ffbb204b
brueck1988 <brueck1988@gmail.com>
2021-05-11 19:56:54 -0600

    Complete welcome page

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -33,18 +33,13 @@ RSpec.describe "Welcome page" do
 
       expect(current_path).to eq(root_path)
       expect(page).to_not have_link("Log Out")
-      expect(page).to have_link("I already have an account")
-      expect(page).to have_link("Sign up to be a user ")
+      expect(page).to have_link("New to Viewing Party? Register Here")
     end
 
     it "As a registered user, I can't log in with incorrect credentials" do
       user = User.create(email: "john@appleseed.com", password: "12345" )
 
       visit root_path
-
-      click_link("I already have an account")
-
-      expect(current_path).to eq(new_session_path)
 
       fill_in :email, with: "john@appleseed.com"
       fill_in :password, with: "123456"
