1e728219d064d037572d33bf7967a290a37c8467
brueck1988 <brueck1988@gmail.com>
2021-05-19 12:55:10 -0600

    Added VCR refresh

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -78,5 +78,6 @@ VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
   config.filter_sensitive_data('<moviedb_api_key>') { ENV['moviedb_api_key'] }
+  config.default_cassette_options = { re_record_interval: 7.days }
   config.configure_rspec_metadata!
 end
