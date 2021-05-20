5e84814381ecf9175cf86b229cef397d8e9ccf71
r-stemmle <ryan.stemmle@gmail.com>
2021-05-20 11:44:38 -0400

    Add production config for assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #help heroku find an image
+  config.assets.compile = true
 end
