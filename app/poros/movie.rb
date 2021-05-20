976aeba3e885cda4af0e85ce53dc46cc09206aa0
r-stemmle <ryan.stemmle@gmail.com>
2021-05-19 22:15:38 -0400

    Refactor poros services controller

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -2,12 +2,24 @@ class Movie
   attr_reader :title,
               :id,
               :vote_average,
-              :overview
+              :overview,
+              :cast,
+              :genres,
+              :runtime
 
   def initialize(data)
     @title = data[:title]
     @id = data[:id]
     @vote_average = data[:vote_average]
     @overview = data[:overview]
+    if data[:credits].nil?
+      @cast
+      @genres
+      @runtime
+    else
+      @cast = data[:credits][:cast].first(10).map { |c| c[:name] }
+      @genres = data[:genres].map { |g| g[:name] }.join(', ')
+      @runtime = data[:runtime]
+    end
   end
 end
--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -16,5 +16,18 @@ class MovieService
     JSON.parse(response.body, symbolize_names: true)[:results]
   end
 
+  def self.search(params)
+    movies_info_1 = parse(conn.get("/3/search/movie?api_key=#{ENV['moviedb_api_key']}&language=en-US&query=#{params}&include_adult=false&page=1"))
+    movies_info_2 = parse(conn.get("/3/search/movie?api_key=#{ENV['moviedb_api_key']}&language=en-US&query=#{params}&include_adult=false&page=2"))
+    movies_info_1 + movies_info_2
+  end
 
+  def self.movie(movie_id)
+    movie_response = conn.get("/3/movie/#{movie_id}?api_key=#{ENV['moviedb_api_key']}&language=en-US&include_adult=false&append_to_response=credits")
+    JSON.parse(movie_response.body, symbolize_names: true)
+  end
+
+  def self.review(movie_id)
+    parse(conn.get("/3/movie/#{movie_id}/reviews?api_key=#{ENV['moviedb_api_key']}&language=en-US&include_adult=false"))
+  end
 end
